0ed481ed110cd87c73df3b76d9a69e3a06d23330
theobori <theo1.bori@epitech.eu>
2022-10-04 16:26:49 +0200

    [ADD / FIX] Added search comand
    Fixed Page component system

--- src/commands/asset/search.ts
+++ src/commands/asset/search.ts
@@ -1,9 +1,11 @@
 import {
+  APIEmbedField,
   ApplicationCommandOption,
   ApplicationCommandOptionType,
   CacheType,
   CommandInteraction,
   CommandInteractionOption,
+  EmbedBuilder,
   Message,
 } from 'discord.js';
   
@@ -11,6 +13,8 @@ import { Bot } from '../../bot';
 import ICommand from '../../interfaces/command';
 import AbstractPageComponent from '../../services/components/page';
 import Teedata from '../../services/apis/teedata';
+import capitalize from '../../utils/capitalize';
+import ErrorEmbed from '../../utils/msg';
 
 type SearchResult = {
   id: number;
@@ -21,11 +25,47 @@ type SearchResult = {
 
 class SearchPageComponent 
 extends AbstractPageComponent<SearchResult> {
-  constructor() {
+  private readonly keyword: string;
+
+  constructor(keyword: string) {
     super();
+
+    this.keyword = keyword;
+  }
+
+  private autoCreateFields(): APIEmbedField[] {
+    const currentContent = this.getCurrentContent();
+    const model = currentContent[0] || [];
+
+    return Object.keys(model).map(name => {
+      return {
+        name: capitalize(name),
+        value: currentContent.map(content => { 
+          return content[name].toString()
+        }).join('\n'),
+        inline: true
+    };
+    });
+  }
+
+  createEmbed(): EmbedBuilder {
+    return new EmbedBuilder()
+      .setTitle(this.keyword)
+      .setColor(0x000000)
+      .addFields(this.autoCreateFields()
+      )
+      .setFooter(
+        {
+          text: 'Page ' + this.pageNumber + ' / ' + this.pageMax
+        }
+      );
+  }
+
+  async reply() {
+    await this.message.followUp(this.createOptions());
   }
 }
-    
+
 export default class implements ICommand {
   name: String;
   category: String;
@@ -35,13 +75,15 @@ export default class implements ICommand {
   constructor() {
     this.name = 'search';
     this.category = 'asset';
-    this.description = 'Find every asset containing `name`';
+    this.description = 'Find every asset containing a keyword';
     this.options = [
       {
-        name: 'name',
+        name: 'keyword',
         type: ApplicationCommandOptionType.String,
         required: true,
-        description: 'The asset\'s name'
+        description: 'The keyword to search inside the assets name',
+        minLength: 3,
+        maxLength: 10
       }
     ];
   }
@@ -49,13 +91,35 @@ export default class implements ICommand {
   async run(
     _bot: Bot,
     message: Message<boolean> | CommandInteraction<CacheType>,
-    _args: Array<CommandInteractionOption>
+    args: Array<CommandInteractionOption>
   ) {
-      await message.reply(
-      {
-        content: 'search'
+      const [ keyword ] = args.map(x => x.value.toString());
+      const interaction = message as CommandInteraction<CacheType>;
+
+      await interaction.deferReply();
+
+      const searchResult = await Teedata.assetSearch(keyword);
+
+      if (searchResult === null) {
+        await interaction.followUp({ embeds: [ ErrorEmbed.notFound() ]});
+        return;
       }
-    );
+
+      const results = searchResult.map((result: SearchResult) => {
+          return {
+            id: '**' + result.id + '**',
+            name: result.name,
+            type: result.type
+          };
+      });
+
+      const component = new SearchPageComponent(keyword)
+        .setMaxLines(10)
+        .setMessage(interaction)
+        .addContent(results);
+    
+    await component.collect();
+    await component.reply();
   };
 }
   
--- src/services/components/page.ts
+++ src/services/components/page.ts
@@ -1,15 +1,19 @@
 import {
-  Message,
   ActionRowBuilder,
   ButtonBuilder,
-  ButtonStyle
+  ButtonStyle,
+  EmbedBuilder,
+  MessagePayload,
+  WebhookEditMessageOptions,
+  CommandInteraction,
+  CacheType
 } from 'discord.js';
 import IPageComponent from '../../interfaces/pageComponent';
 
 import Page from '../../utils/page';
 
-abstract class AbstractPageComponent<T> extends Page<T> implements IPageComponent{
-  protected message: Message<boolean>;
+abstract class AbstractPageComponent<T> extends Page<T> implements IPageComponent {
+  protected message: CommandInteraction<CacheType>;
 
   protected previousId: string = 'previous';
   protected nextId: string = 'next';
@@ -18,7 +22,7 @@ abstract class AbstractPageComponent<T> extends Page<T> implements IPageComponen
     super(maxLines);
   }
 
-  setMessage(message: Message<boolean>): this {
+  setMessage(message: CommandInteraction<CacheType>): this {
     this.message = message;
     
     return this;
@@ -46,12 +50,53 @@ abstract class AbstractPageComponent<T> extends Page<T> implements IPageComponen
 			);
   }
 
-  collect() {
+  createEmbed(): EmbedBuilder {
+    return new EmbedBuilder();
+  }
 
+  protected createOptions()
+  : string | MessagePayload | WebhookEditMessageOptions {
+    return {
+      embeds: [ this.createEmbed() ],
+      components: [ this.createButtons() ]
+    };
+  }
+
+  async collect() {
+    const collector = this.message.channel.createMessageComponentCollector(
+      {
+        time: 1000 * 60
+      }
+    );
+
+    collector.on('collect', async i => {
+      if (i.user.id !== this.message.member.user.id) {
+        return;
+      }
+
+      switch (i.customId) {
+        case this.previousId:
+          this.previous()
+          break;
+        case this.nextId:
+          this.next();
+          break;
+        default:
+          break;
+      }
+
+      try {
+        await i.deferUpdate();
+      } catch (e) {
+        // Log if error
+      }
+      await i.editReply(this.createOptions());
+      
+    });
   }
 
   async reply() {
-
+    await this.message.reply(this.createOptions());
   }
 }
 
